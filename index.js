cd95595722474ed94770c2138d9956fa1eec1bb7
Josue Rios <josuerd89@gmail.com>
2018-01-18 15:47:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var recipes={dessert:"Cookie"}
 function updateObjectWithKeyAndValue(object, key, value){
-var  OBJ= Object.assign({}, object, { [key]: value })
-  return OBJ
+return Object.assign({}, object, { [key]: value })
 }
