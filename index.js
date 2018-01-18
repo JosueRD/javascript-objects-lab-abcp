7f6553f0cf2e1597b50b4e694b82901c18a9d849
Josue Rios <josuerd89@gmail.com>
2018-01-18 15:54:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
   return object
 }
 function deleteFromObjectByKey(object,key){
-  delete object[key]
+  var newobj=object
+  delete newobj.key
+  return newobj
+  
 }
